a098c0435e8230aeefa87c6eeafd31675c2ba8a7
Mya Turner <68873697+MyaTurner@users.noreply.github.com>
2022-09-19 13:54:15 -0400

    created a EnterTwitterHandle component

--- screens/ActionScreen.js
+++ screens/ActionScreen.js
@@ -4,49 +4,64 @@ import { Text, View, ActivityIndicator, TextInput } from "react-native";
 import styles from "../styles";
 import fakeData from "../fakeData";
 
+// Get fake data
 const data = fakeData();
 
+// Define components
 const Separator = () => <View style={styles.separator} />;
+
+const EnterTwitterHandle = ({ handle, setHandle }) => (
+  <View style={styles.viewAction}>
+    <Text style={styles.actionTitle}>ENTER THE TWITTER HANDLE</Text>
+
+    <TextInput
+      style={styles.actionText}
+      placeholder="@"
+      onChangeText={(givenHandle) => setHandle(givenHandle)}
+      defaultValue={handle}
+    ></TextInput>
+  </View>
+);
 
 const ActionScreen = () => {
   const [handle, setHandle] = useState("");
   const [showLoading, setShowloading] = useState(true);
 
   // Find the user information for given twitter handle
-
   if (handle in data) {
     const userInfo = data[handle];
     console.log(userInfo);
   }
   return (
     <View style={styles.viewAction}>
-      <Text style={styles.actionTitle}>ENTER THE TWITTER HANDLE</Text>
-
-      <TextInput
-        style={styles.actionText}
-        placeholder="@"
-        onChangeText={(givenHandle) => setHandle(givenHandle)}
-        defaultValue={handle}
-      ></TextInput>
+      <EnterTwitterHandle
+        setHandle={setHandle}
+        handle={handle}
+      ></EnterTwitterHandle>
 
       <Separator />
       <Separator />
       <Separator />
       <Separator />
-
-      <Text style={styles.actionText}>
-        BOT OR NOT?: {/*userInfo.botOrnot */}
-      </Text>
-      <ActivityIndicator />
-      <Separator />
-      <Text style={styles.actionText}>
-        POSITIVE OR NEGATIVE?: {/*userInfo.posOrneg */}
-      </Text>
-      <Separator />
-      <Text style={styles.actionText}>
-        AVERAGE TOPIC?: {/*userInfo.avgTopic */}
-      </Text>
-      <Separator />
+      {showLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <>
+          <Text style={styles.actionText}>
+            BOT OR NOT?: {/*userInfo.botOrnot */}
+          </Text>
+          <ActivityIndicator />
+          <Separator />
+          <Text style={styles.actionText}>
+            POSITIVE OR NEGATIVE?: {/*userInfo.posOrneg */}
+          </Text>
+          <Separator />
+          <Text style={styles.actionText}>
+            AVERAGE TOPIC?: {/*userInfo.avgTopic */}
+          </Text>
+          <Separator />
+        </>
+      )}
     </View>
   );
 };
